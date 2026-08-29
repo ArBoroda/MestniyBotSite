c9b6171231e6fb5cd846f14cd140cf3ec9ff6e5f
ArBoroda <ravioli9362@gmail.com>
2026-08-29 07:31:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const BOT_USERNAME = "YOUR_BOT_USERNAME";
 
-const botUrl = `https://t.me/${OfficeMestniyBot}`;
+const botUrl = `https://t.me/${BOT_USERNAME}`;
 
 document.querySelectorAll(".bot-link").forEach((link) => {
   link.href = botUrl;
